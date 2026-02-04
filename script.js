dc60f2c8031ea797b3fe288ae4f6588811a4e4fe
Naill10 <abbadnailun28ganz@gmail.com>
2026-02-04 22:13:25 +0700

    FAMOUSS

--- script.js
+++ script.js
@@ -157,6 +157,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-list a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(a => {
+    a.classList.remove("active");
+    if (a.getAttribute("href") === "#" + current) {
+      a.classList.add("active");
+    }
+  });
+});
+
 
 
 
